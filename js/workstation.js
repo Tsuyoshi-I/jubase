e8d32c9f129031ec3458a95afaed24fecd8e5dfd
Tsuyoshi-I <tsuyoshifl244@gmail.com>
2020-04-22 01:24:14 +0900

    間違えたわコメントしまくったのこっちだわ

--- js/workstation.js
+++ js/workstation.js
@@ -1,16 +1,45 @@
-const audioImport = document.getElementById('audioImportBtn')
-const trackList = document.getElementById('trackList')
-const playBtn = document.getElementById('playBtn')
-const pauseBtn = document.getElementById('pauseBtn')
-const stopBtn = document.getElementById('stopBtn')
-const velSlider = document.getElementById('velSlider')
-const audioCtx = new AudioContext()
-const gainNode = audioCtx.createGain()
+// ゴリゴリにグローバルスコープ使ってたりクラス使ってなかったりするのはご愛嬌。（リファクタリングの時間ない）
 
+// インポート・レンダリング
+const audioImport = document.getElementById('audioImportBtn')// ローカル音声ファイルインポート用ボタン
+const trackList = document.getElementById('trackList')// 音源(audioタグ)貼っ付ける場所 ulタグ
+
+// 再生・停止
+const playBtn = document.getElementById('playBtn')// 全体(以下master)再生ボタン
+const pauseBtn = document.getElementById('pauseBtn')// master一時停止ボタン
+const stopBtn = document.getElementById('stopBtn')// master停止ボタン
+
+// master音量コントロールスライダー
+const velSlider = document.getElementById('velSlider')// master音量スライダー
+const velOutput = document.getElementById('vel')// 数値表示用outputタグ
+velOutput.textContent = 0 // この辺は初期化
+velSlider.value = 1
+velSlider.min = -1.0
+velSlider.max = 3.4
+velSlider.step = 0.01
+
+// コンテキスト・APIノード
+const reader = new FileReader()// ローカルファイル読み込み用API
+const audioCtx = new AudioContext()// webAudioApiコンテキスト
+const gainNode = audioCtx.createGain()// 音量用ノード作成
+
+// trackListに入れるliタグのid管理(お粗末)
 let idManege = 0
 
-const tracks = [] // ここにトラックが全部入る
+// 音源管理 ざっくりこんな構造
+//[{音源要素1, プロパティ1, プロパティ2}, {音源要素2...}]→プロパティは随時追加
+const tracks = []
 
+/**
+ * 【track用要素作成＆整形(※HTMLへのレンダリングはしない)】
+ * liタグ・audioタグ・pタグを作ってli > p + audioにする。
+ * pタグはトラック名表示用。className付与。
+ * liにはid付与(idManege)。idManegeは加算。
+ * renderTrack内のreader.onload内で、各要素を渡すために実行。
+ * 引数audioDataはrenderTrack内にて、音声ファイルが渡される。
+ * @param  {File} 
+ * @return {Element, Element} 
+ */
 const createElement = (audioData) => {
   // li作ってクラスとid当ててid加算
   const trackElement = document.createElement('li')
@@ -29,11 +58,15 @@ const createElement = (audioData) => {
   trackElement.appendChild(audioElement)
   trackName.textContent = audioData.name
 
-  return [trackElement, audioElement, trackName]
+  return [trackElement, audioElement]
 }
 
+/**
+ * 【ファイル読み込み→ノード接続・ファイル格納to配列→レンダリング】
+ * @param  {event object(input:input)}
+ * @return {undefined} 
+ */
 const renderTrack = (e) => {
-  // これは絶対クリックされたらの処理
   // 選択されたファイルを変数audioDataに格納＆ファイル種別チェック
   const audioData = e.target.files[0]
   if (!audioData.type.match('audio.*')) {
@@ -41,28 +74,38 @@ const renderTrack = (e) => {
     return
   }
   // audioDataの中身を解析して要素に当てるための処理。
-  const reader = new FileReader()
+  // const reader = new FileReader()
   // onloadが働くのはreadAsDataURLが成功したら
   reader.onload = () => {
-    const [trackElement, audioElement, trackName] = createElement(audioData)
-    audioElement.src = reader.result // srcにファイルソースをセット
+    const [trackElement, audioElement] = createElement(audioData)
+    audioElement.src = reader.result // audioタグのsrcにファイルソースをセット
 
+    // 音声ソース作成
     const trackCtx = audioCtx.createMediaElementSource(trackElement.children[1])
-    trackCtx.connect(audioCtx.destination)// ↓
+
+    // 音声ソースを各ノードに接続
+    // 末尾(audioCtx.destination)はスピーカだと思えばいい
+    // それまでは各処理に必要なエフェクター系
     trackCtx.connect(gainNode).connect(audioCtx.destination)
+
+    // ソース管理用配列(tracks)にオブジェクトの形式で格納
     tracks.push({
       trackElement: trackElement,
     })
+
+    // trackElement(liタグ)の子要素としてaudioタグをぶち込む
     tracks.forEach(track => trackList.appendChild(track.trackElement))
-    audioImport.value = "" // こいつのお陰で同じファイルを連続選択出来る
+    audioImport.value = "" // こいつのお陰で同じファイルを連続選択出来る(※valueが保持されると同じファイルを選択してもinputイベントが発火しない)
   }
   reader.readAsDataURL(audioData)
 }
-
 audioImport.addEventListener('input', renderTrack, false)
 
-// 以下再生処理
-
+/**
+ * 【track一括同時再生】
+ * forEachでtracks内のソースを回してるだけ。
+ * メソッドはwebAudioApiのplay()。
+ */
 const playAudio = () => {
   if (audioCtx.state === 'suspended') {// suspended 一時停止中
     audioCtx.resume();// 一時停止中のものを再開
@@ -71,14 +114,24 @@ const playAudio = () => {
 }
 playBtn.addEventListener('click', playAudio)
 
-// 以下一時停止処理
+/**
+ * 【track一括同時一時停止(中断)】
+ * forEachでtracks内のソースを回してるだけ。
+ * メソッドはwebAudioApiのpause()。
+ * 再開時は止めたところから。
+ */
 const pauseAudio = () => {
-  console.log('うんち')
   tracks.forEach(track => track.trackElement.children[1].pause())
 }
 pauseBtn.addEventListener('click', pauseAudio)
 
-// 以下停止処理
+/**
+ * 【track一括同時停止】
+ * forEachでtracks内のソースを回してるだけ。
+ * メソッドはwebAudioApiのpause()。
+ * 再生位置(currentTime)を0にすることで停止処理にしている。
+ * 再開は最初から。
+ */
 const stopAudio = () => {
   console.log('止まり給う')
   tracks.forEach(track => track.trackElement.children[1].currentTime = 0)
@@ -86,13 +139,11 @@ const stopAudio = () => {
 }
 stopBtn.addEventListener('click', stopAudio)
 
-// velocityスライダー
-const velOutput = document.getElementById('vel')
-velOutput.textContent = 0
-velSlider.value = 1
-velSlider.min = -1.0
-velSlider.max = 3.4
-velSlider.step = 0.01
+/**
+ * 【track一括音量調整】
+ * 該当箇所にスライダーのvalueを入れてるだけ。
+ * ２行目は数値表示。
+ */
 const setSliderValue = () => {
   gainNode.gain.value = velSlider.value
   velOutput.textContent = velSlider.value
