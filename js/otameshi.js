32657361811df3e985dac6be9bb0592df305b0b7
Tsuyoshi-I <tsuyoshifl244@gmail.com>
2020-04-21 16:31:18 +0900

    連続再生時のバグ解消・一時停止、停止機能完成

--- js/otameshi.js
+++ js/otameshi.js
@@ -6,12 +6,6 @@ const audioCtx = new AudioContext()
 let idManege = 0
 
 const tracks = [] // ここにトラックが全部入る
-// tracks = [
-// {
-//   trackElement: trackElemtnt,
-//   
-// },
-// ]
 
 const createElement = (audioData) => {
   // li作ってクラスとid当ててid加算
@@ -48,40 +42,30 @@ const renderTrack = (e) => {
   reader.onload = () => {
     const [trackElement, audioElement, trackName] = createElement(audioData)
     audioElement.src = reader.result // srcにファイルソースをセット
-    tracks.push({
-      trackElement: trackElement
-    })
 
+    const trackCtx = audioCtx.createMediaElementSource(trackElement.children[1])
+    trackCtx.connect(audioCtx.destination)// ↓
+    tracks.push({
+      trackElement: trackElement,
+    })
     tracks.forEach(track => trackList.appendChild(track.trackElement))
     audioImport.value = "" // こいつのお陰で同じファイルを連続選択出来る
   }
   reader.readAsDataURL(audioData)
-
 }
 
 audioImport.addEventListener('input', renderTrack, false)
 
 // 以下再生処理
-// 以下は確か再生しなければ関係ない。一旦置いておいて再考。
 const playAudio = () => {
-  const audio1 = tracks[0].trackElement.children[1]
-  const audio2 = tracks[1].trackElement.children[1]
-  const trackCtx1 = audioCtx.createMediaElementSource(audio1)
-  const trackCtx2 = audioCtx.createMediaElementSource(audio2)
-  trackCtx1.connect(audioCtx.destination)
-  trackCtx2.connect(audioCtx.destination)
-
   if (audioCtx.state === 'suspended') {
     audioCtx.resume();
   }
-  // play or pause track depending on state
   if (playBtn.dataset.playing === 'false') {
-    audio1.play();
-    audio2.play();
+    tracks.forEach(track => track.trackElement.children[1].play())
     playBtn.dataset.playing = 'true';
   } else if (playBtn.dataset.playing === 'true') {
-    audio1.pause();
-    audio2.pause();
+    tracks.forEach(track => track.trackElement.children[1].play())
     playBtn.dataset.playing = 'false';
   }
 }
--- js/workstation.js
+++ js/workstation.js
@@ -1,62 +1,93 @@
 const audioImport = document.getElementById('audioImportBtn')
 const trackList = document.getElementById('trackList')
+const playBtn = document.getElementById('playBtn')
+const pauseBtn = document.getElementById('pauseBtn')
+const stopBtn = document.getElementById('stopBtn')
+const audioCtx = new AudioContext()
+// const source = audioCtx.createBufferSource()
+
 let idManege = 0
 
-const createTrack = (e) => {
+const tracks = [] // ここにトラックが全部入る
+
+const createElement = (audioData) => {
+  // li作ってクラスとid当ててid加算
   const trackElement = document.createElement('li')
   trackElement.classList.add("trackIndex")
   trackElement.id = `track${idManege}`
   idManege++
 
+  // audio作成
   const audioElement = document.createElement('audio')
-
+  // トラック名用pタグ作成
   const trackName = document.createElement('p')
   trackName.classList.add('trackName')
 
+  // 組み立て
+  trackElement.appendChild(trackName)
+  trackElement.appendChild(audioElement)
+  trackName.textContent = audioData.name
+
+  return [trackElement, audioElement, trackName]
+}
+
+const renderTrack = (e) => {
+  // これは絶対クリックされたらの処理
+  // 選択されたファイルを変数audioDataに格納＆ファイル種別チェック
   const audioData = e.target.files[0]
   if (!audioData.type.match('audio.*')) {
     alert('音声を選択してください')
     return
   }
-
+  // audioDataの中身を解析して要素に当てるための処理。
   const reader = new FileReader()
+  // onloadが働くのはreadAsDataURLが成功したら
   reader.onload = () => {
-    audioElement.src = reader.result
-    console.log(audioData.name)
-    trackName.textContent = audioData.name
-    trackElement.appendChild(trackName)
-    trackElement.appendChild(audioElement)
-    trackList.appendChild(trackElement)
+    const [trackElement, audioElement, trackName] = createElement(audioData)
+    audioElement.src = reader.result // srcにファイルソースをセット
+
+    const trackCtx = audioCtx.createMediaElementSource(trackElement.children[1])
+    trackCtx.connect(audioCtx.destination)// ↓
+    tracks.push({
+      trackElement: trackElement,
+
+    })
+    tracks.forEach(track => trackList.appendChild(track.trackElement))
+    audioImport.value = "" // こいつのお陰で同じファイルを連続選択出来る
   }
   reader.readAsDataURL(audioData)
 }
 
-audioImport.addEventListener('input', createTrack, false)
-
+audioImport.addEventListener('input', renderTrack, false)
 
 // 以下再生処理
-
-const playBtn = document.getElementById('playBtn')
-
 const playAudio = () => {
-  const audioCtx = new AudioContext()
-  const audioElements = document.querySelectorAll('#trackList audio')[0]//とりあえず
-  const trackCtx = audioCtx.createMediaElementSource(audioElements)
-  trackCtx.connect(audioCtx.destination)
-
-  if (audioContext.state === 'suspended') {
-    audioContext.resume();
+  if (audioCtx.state === 'suspended') {// suspended 一時停止中
+    audioCtx.resume();// 一時停止中のものを再開
   }
-
-  // play or pause track depending on state
-  if (playBtn.dataset.playing === 'false') {
-    audioElement.play();
-    playBtn.dataset.playing = 'true';
-  } else if (playBtn.dataset.playing === 'true') {
-    audioElement.pause();
-    playBtn.dataset.playing = 'false';
-  }
+  tracks.forEach(track => track.trackElement.children[1].play())
 }
 playBtn.addEventListener('click', playAudio)
 
 
+// 以下一時停止処理
+const pauseAudio = () => {
+  console.log('うんち')
+  tracks.forEach(track => track.trackElement.children[1].pause())
+}
+pauseBtn.addEventListener('click', pauseAudio)
+
+// 以下停止処理
+const stopAudio = () => {
+  console.log('止まり給う')
+  tracks.forEach(track => track.trackElement.children[1].currentTime = 0)
+  tracks.forEach(track => track.trackElement.children[1].pause())
+}
+stopBtn.addEventListener('click', stopAudio)
+
+// audioCtx.decodeAudioData(, function (buffer) {
+//   source.buffer = buffer;
+
+//   source.connect(audioCtx.destination);
+//   source.loop = true;
+// })
